fed5f24f639cec9d94ca7cdacb7b56d0c8971774
eliavra <eliavra691@gmail.com>
2019-11-06 11:45:11 +0200

    3 files modified, 1 deleted

--- src/app/mario.directive.ts
+++ src/app/mario.directive.ts
@@ -1,4 +1,4 @@
-import {HostListener, Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 
 @Directive({
@@ -6,24 +6,50 @@ import {HostListener, Directive } from '@angular/core';
 })
 export class MarioDirective {
 
-  constructor() {
-    this.sound = new Audio('');
+  constructor(private el: ElementRef) {
+    this.sound = new Audio('https://vocaroo.com/media_command.php?media=s0aMPUPHtPZX&command=download_mp3');
    }
 
 private sound;
 private marioData = {
     song: 'https://www.myinstants.com/media/sounds/untitled_3.mp3',
-    image: 'https://upload.wikimedia.org/wikipedia/he/a/a9/MarioNSMBUDeluxe.png',
+    image: 'https://www.hbo.com/content/dam/hbodata/series/silicon-valley/characters/s3/gilfoyle-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg',
     color: '#E52521'
   };
 
 @HostListener('mouseenter') onMouseEnter()
 {
-  this.sound.play();
+    this.sound.play();
+    this.addImage();
+    this.changeColor(this.marioData.color);
 }
 
 @HostListener('mouseleave') onMouseLeave()
 {
-  this.sound.stop();
+    this.sound.pause();
+    this.sound.currentTime = 0;
+    this.removeImage();
+    this.changeColor(null);
 }
+
+  private addImage() {
+    let img = document.createElement("img");
+    const el = this.el.nativeElement;
+    img.id = 'mario-img';
+    img.src = this.marioData.image;
+    img.style.cssText = `width: 300px;
+                        display: block`;
+    el.parentNode.insertBefore(img, el.nextSibling);
+  }
+
+
+  private removeImage() {
+    document.getElementById("mario-img").remove();
+  }
+
+  private changeColor(color) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+  
+
 }
